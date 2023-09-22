4358db95470c047bd36411126fc005134cfc0960
xingliu <liuxingcoco@iCloud.com>
2023-09-22 17:29:54 +0800

    feat: stark

--- src/views/home-page-module/home-style-page.ts
+++ src/views/home-page-module/home-style-page.ts
@@ -2,8 +2,39 @@ import styled from 'styled-components'
 
 
 export const HomeWallpaper = styled.div`
-  filter: drop-shadow(0 0 2em #835F99);
   color: #333;
   height: 100%;
   width: 100%;
+  .workbench {
+    height: inherit;
+    flex-direction: column;
+    .ant-image {
+      border-radius: 50%;
+    }
+    .workbench-gif {
+      border-radius: 50%;
+      pointer-events: none;
+      filter: drop-shadow(0 0 3em #d76ac3);
+    }
+    .sys-stark {
+      padding-top: 30px;
+      text-align: center;
+      flex-direction: column;
+      .sys-hello {
+        padding-top: 5px;
+        color: #fff;
+        font-size: 1.5em;
+        font-weight: 900;
+      }
+      .stark-start {
+        cursor: pointer;
+        width: 80px;
+        height: 80px;
+        color: #fff;
+        background-color: #8178a3;
+        border-radius: 50%;
+        filter: drop-shadow(0 0 3em #6addff);
+      }
+    }
+  }
 `
--- src/views/home-page-module/index.tsx
+++ src/views/home-page-module/index.tsx
@@ -1,10 +1,22 @@
-import React, { memo, Suspense } from 'react'
+import React, { memo } from 'react'
 import { HomeWallpaper } from './home-style-page'
+import { Button, Image, Space } from 'antd';
+
+import workGif from '@/assets/image/gif/home.gif'
 export default memo(function JMSHomeMain() {
+  
   return (
     <>
       <HomeWallpaper>
-        这里是首页
+        <div className='workbench flex-center'>
+          <Image width={300} src={workGif} preview={false} alt="" className='workbench-gif' />
+          <div className='sys-stark flex-center'>
+            <p className='sys-hello'>欢迎使用 Stark X</p>
+            <div className='stark-start flex-center'>
+              <div className='start-cor flex-center'>开启</div>
+            </div>
+          </div>
+        </div>
       </HomeWallpaper>
     </>
   )
